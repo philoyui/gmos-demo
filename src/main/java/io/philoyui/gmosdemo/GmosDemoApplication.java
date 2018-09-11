bac0bb96ef669dfa6cd8f6950ef49225a380e1d3
yang7551735 <yang7551735@126.com>
2018-09-11 23:08:19 +0800

    初始化

---
+++ src/main/java/io/philoyui/gmosdemo/GmosDemoApplication.java
@@ -0,0 +1,12 @@
+package io.philoyui.gmosdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmosDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmosDemoApplication.class, args);
+	}
+}
